32b06827e85adfead79a4b45d437ed23fc16f770
Ragazzi147 <lucasax147@gmail.com>
2025-10-14 17:10:51 -0300

    Feat: perguntas toggle

--- js/script.js
+++ js/script.js
@@ -30,3 +30,26 @@ parametros.forEach(ativarProduto)
 
 console.log(parametros)
 
+
+// perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button')
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget
+    const controls = pergunta.getAttribute('aria-controls')
+    const resposta = document.getElementById(controls)
+
+
+
+    resposta.classList.toggle('ativa')
+    const ativa = resposta.classList.contains('ativa')
+    pergunta.setAttribute('aria-expanded', ativa)
+    
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta)    
+}
+
+perguntas.forEach(eventosPerguntas)
